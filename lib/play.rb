e22cd1978251fbf0eebe9a3bc00eadcca203d4e7
Garrett Crabb <garrettcrabb@gmail.com>
2016-08-23 22:52:09 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -7,21 +7,26 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end
+
 
 def move(board, index, current_player = "X")
   board[index] = current_player
 end
 
+
 def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
 
+
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
+
 
 def turn(board)
   puts "Please enter 1-9:"
@@ -35,4 +40,11 @@ def turn(board)
   end
 end
 
-# Define your play method below
+
+def play(board)
+  round = 0
+    until round == 9
+      round += 1
+      turn(board)
+    end
+end
