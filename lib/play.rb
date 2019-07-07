4282f6f5d1d96a311264d69a417c4d3f81552404
Tiffany Tang <tifftang22@gmail.com>
2019-07-07 21:33:12 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -35,4 +35,11 @@ def turn(board)
   end
 end
 
-# Define your play method below
+# Play method below
+def play(board)
+  total_turns = 0
+  while total_turns < 9
+    turn(board)
+    total_turns += 1
+  end
+end
