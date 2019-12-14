870461ae0d9d8cf324b4847c3690b5fc012adbac
Isaiah Velez <isaiah.velez611@gmail.com>
2019-12-14 07:48:20 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,14 @@ def turn(board)
 end
 
 # Define your play method below
+
+def play(board)
+counter = 0
+ loop do 
+  counter += 1
+  puts turn(board)
+  if counter >= 9 
+    break
+  end
+end
+end 
