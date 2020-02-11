1ca262833fa04431d3124c7d07d0a78f881bb3d3
dazzling-acrobat-7297 <abhips@gmail.com>
2020-02-11 11:23:41 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 # Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
+  puts '-----------'
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
+  puts '-----------'
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
@@ -11,20 +13,20 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
+def move(board, index, current_player = 'X')
   board[index] = current_player
 end
 
 def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+  board[location] != ' ' && board[location] != ''
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
+  puts 'Please enter 1-9:'
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
@@ -36,3 +38,6 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  9.times { turn(board) }
+end
